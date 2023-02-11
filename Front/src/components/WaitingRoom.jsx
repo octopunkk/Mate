95cb16240bce1f342d42909b036893e33c8cd64f
octopunkk <anais.derue@insa-rennes.fr>
2023-02-11 22:53:30 +0100

    add style and stuff to waiting room

--- Front/src/components/WaitingRoom.jsx
+++ Front/src/components/WaitingRoom.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
 import server from "../utils/server";
 import Avatar from "boring-avatars";
+import "./WaitingRoom.css";
 
 function WaitingRoom() {
   const [roomId, setRoomId] = useState("");
   const [user, setUser] = useState({});
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(roomId);
+  };
 
   useEffect(() => {
     const getUser = async () => {
@@ -23,8 +28,25 @@ function WaitingRoom() {
   }, []);
 
   return (
-    <div>
-      <h3>Room id : {roomId}</h3>
+    <div className="WaitingRoom">
+      <h2>Les joueurs peuvent rejoindre la partie</h2>
+      <h3>Joueurs dans la partie : </h3>
+      <p className="player">
+        <Avatar
+          size={30}
+          name={user.userId}
+          variant="beam"
+          colors={["#F6D76B", "#FF9036", "#D6254D", "#FF5475", "#FDEBA9"]}
+        />{" "}
+        {user.displayName} (toi)
+      </p>
+      <div className="roomId">
+        <h3>Code de la partie :</h3>
+        <button className="tooltip" onClick={copyToClipboard}>
+          {roomId}
+          <p className="tooltiptext">Copier</p>
+        </button>
+      </div>
     </div>
   );
 }
